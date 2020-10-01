00e217d839e99375c5819a1b135c0334aef32492
PrayagRana <56391007+PrayagRana@users.noreply.github.com>
2020-10-01 16:42:17 +0530

    Add files via upload

---
+++ Nonpareil/nonpareil.js
@@ -0,0 +1,48 @@
+function myFunction1() {
+        var copyText = document.getElementById("coupon1");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction2() {
+        var copyText = document.getElementById("coupon2");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction3() {
+        var copyText = document.getElementById("coupon3");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction4() {
+        var copyText = document.getElementById("coupon4");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction5() {
+        var copyText = document.getElementById("coupon5");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction6() {
+        var copyText = document.getElementById("coupon6");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction7() {
+        var copyText = document.getElementById("coupon7");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
+      function myFunction8() {
+        var copyText = document.getElementById("coupon8");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
